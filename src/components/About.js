e54f257d4cba004eef7909e0da8064cb94ce16c6
Mammet-tech <edwinmammet19@gmail.com>
2025-04-21 00:11:25 +0300

    Complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,18 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2> About Me </h2>
+      <p>
+        I am a web developer with a passion for creating beautiful and
+        functional user experiences. I love to learn new technologies and
+        improve my skills. In my free time, I enjoy hiking, reading, and
+        spending time with my family.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
